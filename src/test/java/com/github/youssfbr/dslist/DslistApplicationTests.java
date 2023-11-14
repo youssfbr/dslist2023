7c27cc19647c54c11008f822ac95a84f72e835e1
Alisson Youssf <youssfbr@gmail.com>
2023-11-14 00:23:59 -0300

    Initial commit

---
+++ src/test/java/com/github/youssfbr/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
