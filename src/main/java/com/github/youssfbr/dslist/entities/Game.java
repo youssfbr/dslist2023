cc82111b0dd6044d92c644750d8cefaa4706fb62
Alisson Youssf <youssfbr@gmail.com>
2023-11-14 00:57:27 -0300

    Properties, Game, ORM

---
+++ src/main/java/com/github/youssfbr/dslist/entities/Game.java
@@ -0,0 +1,30 @@
+package com.github.youssfbr.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tb_game")
+@EqualsAndHashCode(of = "id")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+
+}
