816f7e0a1a5f37a04687b9ae10d26499b679c727
Alisson Youssf <youssfbr@gmail.com>
2023-11-18 10:16:46 -0300

    Domain Model

--- src/main/java/com/github/youssfbr/dslist/entities/Game.java
+++ src/main/java/com/github/youssfbr/dslist/entities/Game.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
 
 import java.util.Objects;
 
@@ -34,18 +33,15 @@ public class Game {
     private String longDescription;
 
     @Override
-    public final boolean equals(Object o) {
+    public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
-        return getId() != null && Objects.equals(getId(), game.getId());
+        return Objects.equals(id, game.id);
     }
 
     @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
